a3eef796297c70911e82c053d173245f205fd4e3
Guilherme <guilhermehenrike0412@gmail.com>
2026-08-28 10:54:15 -0300

    feat: base do sistema de login e registro com senha já criptografada utilizando render_template

--- database.py
+++ database.py
@@ -4,6 +4,6 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Gilneide", ## TUA SENHA
+        password="senha", ## TUA SENHA
         database="music_hub"
     )
