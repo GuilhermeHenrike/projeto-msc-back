00f43004e0363ee1eb82aed85e83e310e67f8d6f
Guilherme <guilhermehenrike0412@gmail.com>
2026-09-22 16:59:35 -0300

    feat: adicionando o buscar perfil

--- app/controllers/perfilProfile.py
+++ app/controllers/perfilProfile.py
@@ -1,6 +1,22 @@
-from flask import redirect, request, session
+from flask import redirect, request, session, jsonify
 
 def perfilController(app, perfilService):
+
+    @app.route("/perfil", methods=["GET"])
+    def buscarPerfil():
+
+        if "user.id" not in session:
+            return jsonify({"erro": "Usuário não autenticado"}), 401
+
+        usuario_id = session["user.id"]
+
+        usuario = perfilService.buscarPerfil(usuario_id)
+
+        if not usuario:
+            return jsonify({"erro": "Usuário não encontrado"}), 404
+
+        return jsonify(usuario), 200
+
 
     @app.route("/perfil", methods=["POST"])
     def editarPerfil():
